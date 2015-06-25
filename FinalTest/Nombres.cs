9404b909ce4e8f4c8a5549b385c14646c8389365
MAROUA OTHMAN <MAROUA241@GMAL.COM>
2015-06-25 10:38:53 +0200

    deplacer class nombres

--- FinalTest/Nombres.cs
+++ FinalTest/Nombres.cs
@@ -1,11 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 
-namespace FinalTest.Tests
+namespace FinalTest
 {
     class Nombres
     {
+        private IEnumerable<KeyValuePair<string, int>> _keyValuePairs;
+        public Nombres(IEnumerable<KeyValuePair<string, int>> keyValuePairs)
+        {
+            this._keyValuePairs = keyValuePairs;
+            
+        }
+
+        public IEnumerable<int> NombresPairs 
+        {
+            get
+            {
+                return _keyValuePairs.Where(x => (x.Value % 2) == 0).Select(x => x.Value);
+            }
+         }
+
+        public String TexteNombresImpairs
+        {
+            get { return _keyValuePairs.Where(x => (x.Value % 2) != 0).OrderBy(x =>x.Value)
+                .Select(x => x.Key).Aggregate((current, next) => current + ", " + next); }
+            
+        }
+
+        public String PremierNombreDontLeTexteContientPlusDe5Caractères
+        {
+            get { return _keyValuePairs.Where(x => (x.Key.Length > 5)).Select(x => x.Key).Aggregate((current, next) => current); }
+        }
+
+        public IEnumerable<int> QuatreNombresSupérieursSuivant3
+        {
+            get { return _keyValuePairs.Where(x => (x.Value > 3)).OrderBy(x =>x.Value).Select(x => (x.Value)).Skip(0).Take(4) ; }
+        }
     }
 }
