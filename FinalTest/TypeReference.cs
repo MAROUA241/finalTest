7a85a6a488b9747ee50249a35b21998130ec2652
MAROUA OTHMAN <MAROUA241@GMAL.COM>
2015-06-25 09:09:16 +0200

    second test

---
+++ FinalTest/TypeReference.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTest
+{
+   public class TypeReference
+    {
+    
+
+     
+
+       private  int _valeur;
+
+        public TypeReference(int valeur)
+        {
+
+            this._valeur = valeur;
+        }
+        public override bool Equals(object obj)
+        {
+
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            TypeReference typeReferenceObj = obj as TypeReference;
+
+            if (typeReferenceObj._valeur != this._valeur) return false;
+
+            return true;
+        }
+
+ 
+
+     
+ 
+     
+    }
+}
