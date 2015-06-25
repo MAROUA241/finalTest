b1e1839fc79f00177bc37b3ce0ed018725caaa18
MAROUA OTHMAN <MAROUA241@GMAL.COM>
2015-06-25 11:17:22 +0200

    OuvrirUnCompteBancaireProduitUnEvénemen

---
+++ FinalTest/CompteBancaire.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTest
+{
+    public class CompteBancaire
+    {
+        private static readonly IEnumerable<IEvenementMetier> _initialElements;
+        public static IEnumerable<IEvenementMetier> Ouvrir(string numéroDeCompte, int autorisationDeCrédit)
+        {
+           yield  return new  CompteCréé(numéroDeCompte,autorisationDeCrédit);
+
+        }
+    }
+
+    
+}
---
+++ FinalTest/CompteCréé.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTest
+{
+    public class CompteCréé :IEvenementMetier
+    {
+
+        private string numéroDeCompte ;
+        private int autorisationDeCrédit;
+        public CompteCréé(string numéroDeCompte, int autorisationDeCrédit)
+        {
+            this.numéroDeCompte = numéroDeCompte;
+            this.autorisationDeCrédit = autorisationDeCrédit;
+        }
+
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            CompteCréé compteObj = obj as CompteCréé;
+
+            if (compteObj.numéroDeCompte != this.numéroDeCompte) return false;
+            if (compteObj.autorisationDeCrédit != this.autorisationDeCrédit) return false;
+
+            return true;
+        }
+
+        public IEnumerable<IEvenementMetier> Ouvrir(string numéroDeCompte, int autorisationDeCrédit)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
